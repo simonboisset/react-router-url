ae999f713eae233de84f4e22a14a308f633daa79
Simon Boisset <sboisset@ambuliz.com>
2020-11-08 01:43:36 +0100

    add searchparams for url

--- src/utils/useURL.ts
+++ src/utils/useURL.ts
@@ -13,6 +13,16 @@ export const useURL = () => {
       window.removeEventListener('popstate', handleChangeURL);
     };
   }, []);
-
-  return url;
+  const path = url.pathname;
+  const origin = url.origin;
+  let params: { [key: string]: string | null } = {};
+  const paramsList = url.search
+    .slice(1)
+    .split('&')
+    .map((param) => param.split('='))
+    .map((param) => ({ [param[0]]: url.searchParams.get(param[0]) }));
+  for (const param of paramsList) {
+    params = { ...params, ...param };
+  }
+  return { origin, path, params };
 };
